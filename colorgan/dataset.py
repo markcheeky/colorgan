1229d3f312e358796716b8ff24acb0cf89f2c69e
Marek Kadlcik <200~10684818+markcheeky@users.noreply.github.com>
2022-04-28 18:48:43 +0200

    add working implemenation of TF dataset from generator

--- colorgan/dataset.py
+++ colorgan/dataset.py
@@ -1,100 +1,155 @@
+import concurrent.futures
 import io
-import logging
-from typing import Collection, Iterable, List, Optional, Tuple
+import os
+from typing import Collection, Generator, Optional, Tuple
 
-import grequests
 import numpy as np
-import PIL
+import PIL.Image
 import requests
-import skimage.color
 import tensorflow as tf
+from requests import Response
+from tensorflow.data import Dataset
+from utils import IMG_EXTENSIONS, chunkify
 
 
-class ImgUrlDataset(tf.keras.utils.Sequence):
-    def __init__(
-        self,
-        urls: Collection,
-        batch_size: int = 32,
-        max_width: int = 1080,
-        max_height: int = 1080,
-    ) -> None:
+def response_to_img(response: Response) -> PIL.Image.Image:
 
-        self.urls = urls
-        self.batch_size = batch_size
-        self.max_width = max_width
-        self.max_height = max_height
+    if response.status_code != 200:
+        err = f"Response from {response.url} has code {response.status_code}"
+        raise ValueError(err, response)
 
-    def __len__(self) -> int:
-        return len(self.urls) // self.batch_size
+    img = PIL.Image.open(io.BytesIO(response.content))
+    img_array = np.asarray(img)
 
-    def __getitem__(self, idx: int) -> Tuple[tf.Tensor, tf.Tensor]:
-        start = idx * self.batch_size
-        end = start + self.batch_size
-        rows = self.urls[start:end]
-        imgs = self.fetch_imgs(rows)
-        imgs = [self.resize_to_fit(img, self.max_height, self.max_width) for img in imgs]
-        imgs = [
-            self.pad_to_size(
-                img, self.max_height, self.max_width, 0, np.random.default_rng(idx + i)
-            )
-            for i, img in enumerate(imgs)
-        ]
-        img_bw = [np.expand_dims(skimage.color.rgb2gray(img), -1) for img in imgs]
-        x = (np.stack(img_bw).astype(np.float32) / 255.0 - 0.5) * 2
-        y = (np.stack(imgs).astype(np.float32) / 255.0 - 0.5) * 2
-        # todo return masks
-        return tf.convert_to_tensor(x), tf.convert_to_tensor(y)
+    if len(img_array.shape) != 3:
+        err = f"image {response.url} with shape {img_array.shape} has invalid dimensions."
+        raise ValueError(err, response, img)
 
-    @staticmethod
-    def resize_to_fit(
-        img: np.ndarray,
-        max_height: int,
-        max_width: int,
-    ) -> np.ndarray:
-        pil_img = PIL.Image.fromarray(img)
-        pil_img.thumbnail((max_width, max_height))
-        return np.asarray(pil_img)
+    if img_array.shape[-1] not in (3, 4):
+        err = f"image {response.url} with shape {img_array.shape} has wrong number of channels."
+        raise ValueError(err, response, img)
 
-    @staticmethod
-    def pad_to_size(
-        img: np.ndarray,
-        height: int,
-        width: int,
-        bg_val: int,
-        random_gen: np.random.Generator,
-    ) -> np.ndarray:
+    return img
 
-        t_padding = random_gen.integers(0, height - img.shape[0] + 1)
-        b_padding = height - img.shape[0] - t_padding
-        vertical = (t_padding, b_padding)
 
-        l_padding = random_gen.integers(0, width - img.shape[1] + 1)
-        r_padding = width - img.shape[1] - l_padding
-        horizontal = (l_padding, r_padding)
+def get_img(
+    image_id: str,
+    url: str,
+    max_img_size: Tuple[int, int],
+    use_cache: bool,
+    cache_dir: Optional[str] = None,
+    timeout: int = 10,
+) -> Optional[np.ndarray]:
 
-        return np.pad(img, [vertical, horizontal, (0, 0)], constant_values=bg_val)
+    possible_paths = [f"{cache_dir}/{image_id}.{ext}" for ext in IMG_EXTENSIONS]
+    existings = list(filter(os.path.exists, possible_paths))
 
-    @staticmethod
-    def response_to_img_safe(response: requests.Response) -> Optional[np.ndarray]:
+    if len(existings) != 0:
+        img = PIL.Image.open(existings[0])
+    else:
         try:
-            img = skimage.io.imread(io.BytesIO(response.content))
-            if len(img.shape) != 3:
-                raise ValueError(
-                    f"image {response.url} with shape {img.shape} has invalid dimensions."
-                )
-            if img.shape[-1] not in (3, 4):
-                raise ValueError(
-                    f"image {response.url} with shape {img.shape} has wrong number of channels."
-                )
-            img = img[:, :, :3]  # dropping potential alpha channel
-            return img
+            response = requests.get(url, timeout=timeout)
+            img = response_to_img(response)
+            if use_cache:
+                img.save(f"{cache_dir}/{image_id}.{img.format.lower()}")
         except Exception as e:
-            logging.warning(response.url, e)
-        return None
+            print(e)
+            return None
 
-    @staticmethod
-    def fetch_imgs(urls: Iterable[str]) -> List[np.ndarray]:
-        reqs = (grequests.get(url, timeout=10) for url in urls)
-        responses = grequests.map(reqs)
-        imgs = list(map(ImgUrlDataset.response_to_img_safe, responses))
-        return [img for img in imgs if img is not None]
+    img.thumbnail(max_img_size)
+    array = np.asarray(img)[:, :, :3]
+    return array
+
+
+def download_imgs(
+    image_ids: Collection[str],
+    urls: Collection[str],
+    workers: int,
+    max_img_size: Tuple[int, int],
+    use_cache: bool = False,
+    cache_dir: Optional[str] = None,
+) -> Generator[np.ndarray, None, None]:
+    def getter(img_id: str, url: str) -> Optional[np.ndarray]:
+        return get_img(img_id, url, max_img_size, use_cache, cache_dir)
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as pool:
+        for chunk in chunkify(zip(image_ids, urls), workers):
+            futures = [pool.submit(getter, *args) for args in chunk]
+            for img in concurrent.futures.as_completed(futures):
+                if img.result() is not None:
+                    yield img.result()
+
+
+def get_xy_for_G(img: tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor]:
+    x = tf.image.rgb_to_grayscale(img)
+    x = tf.repeat(x, 3, axis=-1)
+    x = tf.cast(x, tf.float32) / 127.5 - 1
+    y = tf.cast(img, tf.float32) / 127.5 - 1
+    return x, y
+
+
+def preprocess(inputs: np.ndarray) -> np.ndarray:
+    return inputs.astype(np.float32) / 127.5 - 1
+
+
+def postprocess(outputs: np.ndarray) -> np.ndarray:
+    return ((outputs + 1) * 127.5).astype(np.uint8)
+
+
+def extract_patches(
+    img: tf.Tensor,
+    patch_size: Tuple[int, int],
+    stride: Tuple[int, int],
+) -> Dataset:
+
+    img = tf.expand_dims(img, 0)
+    patches = tf.image.extract_patches(
+        images=img,
+        sizes=[1, *patch_size, 1],
+        strides=[1, *stride, 1],
+        rates=[1, 1, 1, 1],
+        padding="VALID",
+    )
+
+    patches = tf.reshape(patches, (-1, *patch_size, 3))
+    return Dataset.from_tensor_slices(patches)
+
+
+def url_dataset_for_G(
+    image_ids: Collection[str],
+    urls: Collection[str],
+    augment: bool = False,
+    use_cache: bool = False,
+    cache_dir: Optional[str] = None,
+    patch_size: Tuple[int, int] = (512, 512),
+    stride: Tuple[int, int] = (400, 400),
+    max_img_size: Tuple[int, int] = (1350, 1350),
+    download_workers: int = 25,
+    prefetch: int = 0,
+    repeats: int = 1,
+    repeat_cycle: int = 5000,
+    seed: int = 42,
+) -> Dataset:
+
+    tf.random.set_seed(seed)
+    signature = tf.TensorSpec(shape=(None, None, 3), dtype=tf.uint8)
+
+    ds = Dataset.from_generator(
+        lambda: download_imgs(
+            image_ids, urls, download_workers, max_img_size, use_cache, cache_dir
+        ),
+        output_signature=signature,
+    )
+
+    ds = ds.prefetch(prefetch)
+    ds = ds.flat_map(lambda img: extract_patches(img, patch_size, stride))
+    ds = ds.interleave(lambda img: Dataset.from_tensors(img).repeat(repeats), repeat_cycle)
+
+    if augment:
+        ds = ds.map(lambda img: tf.image.random_flip_left_right(img))
+        ds = ds.map(lambda img: tf.image.random_brightness(img, max_delta=0.15))
+        ds = ds.map(lambda img: tf.image.random_saturation(img, lower=0.8, upper=1.2))
+
+    ds = ds.map(get_xy_for_G)
+
+    return ds
--- colorgan/utils.py
+++ colorgan/utils.py
@@ -1,14 +1,28 @@
+import itertools
 from pathlib import Path
+from typing import Any, Generator, Iterable
 
 import tensorflow as tf
 from IPython.display import SVG
+from PIL import Image
 
 PROJ_ROOT = Path(__file__).parent.parent
 
+IMG_EXTENSIONS = {
+    ex.strip(".") for ex, f in Image.registered_extensions().items() if f in Image.OPEN
+}
 
-def plot_model(model, expand_nested=False):
+
+def plot_model(model: tf.keras.Model, dpi: int = 64, **kwargs: Any) -> SVG:
     return SVG(
-        tf.keras.utils.model_to_dot(model, dpi=60, expand_nested=expand_nested).create(
-            prog="dot", format="svg"
-        )
+        tf.keras.utils.model_to_dot(model, dpi=dpi, **kwargs).create(prog="dot", format="svg")
     )
+
+
+def chunkify(iterable: Iterable, chunk_size: int) -> Generator[Any, None, None]:
+    it = iter(iterable)
+    while True:
+        chunk = tuple(itertools.islice(it, chunk_size))
+        if not chunk:
+            return
+        yield chunk
