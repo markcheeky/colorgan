e5797854d48e82bb4b035daf3089b43cc7a3c141
Marek Kadlcik <200~10684818+markcheeky@users.noreply.github.com>
2022-04-28 18:51:29 +0200

    implement baseline

--- colorgan/models.py
+++ colorgan/models.py
@@ -1,6 +1,7 @@
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 
 from tensorflow.keras.layers import (
+    Activation,
     BatchNormalization,
     Concatenate,
     Conv2D,
@@ -11,69 +12,103 @@ from tensorflow.keras.layers import (
     Input,
     Layer,
     LeakyReLU,
-    ReLU,
 )
 from tensorflow.keras.models import Model, Sequential
 
 
+def get_unet_encoder_block(
+    inputs: Layer,
+    kernel_num: int,
+    kernel_size: Tuple[int, int],
+    stride: int,
+    batch_norm: bool,
+    leakiness: float = 0.2,
+) -> Sequential:
+
+    block = Sequential()
+
+    block.add(Conv2D(kernel_num, kernel_size, padding="same", strides=stride))
+    if batch_norm:
+        block.add(BatchNormalization())
+    block.add(LeakyReLU(alpha=leakiness))
+
+    return block(inputs)
+
+
+def get_unet_decoder_block(
+    inputs: Layer,
+    kernel_num: int,
+    kernel_size: Tuple[int, int],
+    stride: int,
+    dropout: bool,
+    dropout_rate: float = 0.5,
+    activation: str = "relu",
+) -> Sequential:
+
+    block = Sequential()
+
+    if isinstance(inputs, list):
+        inputs = Concatenate(axis=3)(inputs)
+
+    block.add(Conv2DTranspose(kernel_num, kernel_size, padding="same", strides=stride))
+    block.add(BatchNormalization())
+    if dropout:
+        block.add(Dropout(dropout_rate))
+    block.add(Activation(activation))
+
+    return block(inputs)
+
+
 def get_unet_generator(
-    input_shape: Tuple[Optional[int]] = (None, None, 3),
-    encoder_kernel_nums: Tuple[int] = (64, 128, 256, 512, 512, 512, 512, 512),
-    decoder_kernel_nums: Optional[Tuple[int]] = None,
-    kernel_size: Tuple[int] = (4, 4),
+    input_shape: List[Optional[int]] = [None, None, 3],
+    encoder_kernel_nums: List[int] = [64, 128, 256, 512, 512, 512, 512, 512],
+    decoder_kernel_nums: List[int] = [512, 512, 512, 512, 256, 128, 64, 3],
+    kernel_size: Tuple[int, int] = (4, 4),
     stride: int = 2,
 ) -> Model:
-
-    if decoder_kernel_nums is None:
-        decoder_kernel_nums = encoder_kernel_nums[::-1]
 
     if len(decoder_kernel_nums) != len(encoder_kernel_nums):
         raise ValueError("number of encoder and decoder layers must match")
 
     input_layer = Input(shape=input_shape)
-    encoder_blocks = []
-    decoder_blocks = []
+    encoder_blocks: List[Layer] = []
+    decoder_blocks: List[Layer] = []
 
     for kernel_num in encoder_kernel_nums:
+
         if len(encoder_blocks) == 0:
             inputs = input_layer
+            batch_norm = False
         else:
             inputs = encoder_blocks[-1]
+            batch_norm = True
 
-        block = Sequential(
-            [
-                Conv2D(kernel_num, kernel_size, padding="same", strides=stride),
-                BatchNormalization() if len(encoder_blocks) != 0 else Layer(),
-                LeakyReLU(alpha=0.2),
-            ]
+        encoder_blocks.append(
+            get_unet_encoder_block(inputs, kernel_num, kernel_size, stride, batch_norm)
         )
-
-        encoder_blocks.append(block(inputs))
 
     for skip_src, kernel_num in zip(encoder_blocks[::-1], decoder_kernel_nums):
         if len(decoder_blocks) == 0:
             inputs = skip_src
         else:
-            inputs = Concatenate(axis=3)([skip_src, decoder_blocks[-1]])
+            inputs = [skip_src, decoder_blocks[-1]]
 
-        block = Sequential(
-            [
-                Conv2DTranspose(kernel_num, kernel_size, padding="same", strides=2),
-                BatchNormalization(),
-                Dropout(0.5) if len(decoder_blocks) < 3 else Layer(),
-                ReLU(),
-            ]
+        dropout = len(decoder_blocks) < 3
+        is_last = len(decoder_blocks) == len(decoder_kernel_nums) - 1
+        activation = "tanh" if is_last else "relu"
+
+        decoder_blocks.append(
+            get_unet_decoder_block(
+                inputs, kernel_num, kernel_size, stride, dropout, activation=activation
+            )
         )
 
-        decoder_blocks.append(block(inputs))
-
-    output_layer = Conv2DTranspose(3, kernel_size, activation="tanh")(decoder_blocks[-1])
-    return Model(inputs=input_layer, outputs=output_layer)
+    return Model(inputs=input_layer, outputs=decoder_blocks[-1])
 
 
 def get_discriminator_block(
     kernel_num: int,
-    kernel_size: Tuple[int],
+    kernel_size: Tuple[int, ...],
     stride: int,
     batch_norm: bool,
 ) -> Sequential:
@@ -88,9 +123,9 @@ def get_discriminator_block(
 
 
 def get_discriminator(
-    input_shape: Tuple[Optional[int]] = (None, None, 3),
-    kernel_nums: Tuple[int] = (64, 128, 256, 512),
-    kernel_size: Tuple[int] = (4, 4),
+    input_shape: Tuple[Optional[int], ...] = (None, None, 3),
+    kernel_nums: Tuple[int, ...] = (64, 128, 256, 512),
+    kernel_size: Tuple[int, ...] = (4, 4),
     stride: int = 2,
 ) -> Model:
 
